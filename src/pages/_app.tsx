0102afd20cd68e268268b53f936c51dba15d98fa
Pedro Silva <pedro.silva@somagrupo.com.br>
2020-09-28 13:59:20 -0300

    finsih storybook x.5

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="drescription"
+          content="A simple project starter to work with Typescript,
+          React, NextJS and Styled Components"
+        ></meta>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
